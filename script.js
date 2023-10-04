a0bb6bff7558405598b0cca6946cfbbdd6f59424
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2023-10-04 16:49:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,23 +4,27 @@
 const GRID_SIZE = 40;
 const PIXEL_SIZE = 10;
 
-// Game variables
-let snake = [{ x: 1, y: 20 }]; // Initial snake position
-let direction = 'right';
-let food = { x: Math.floor(Math.random() * GRID_SIZE), y: Math.floor(Math.random() * GRID_SIZE) };
-let score = 0;
-
-// Game loop
-function gameLoop() {
-    // Move the snake based on the direction
-    // Check for collisions and update game state
-    // Update the display
+// Function to create and display a pixel
+function createPixel(x, y, className) {
+    const pixel = document.createElement('div');
+    pixel.className = className;
+    pixel.style.left = `${x * PIXEL_SIZE}px`;
+    pixel.style.top = `${y * PIXEL_SIZE}px`;
+    document.getElementById('gameContainer').appendChild(pixel);
 }
 
-// Handle user input to change the direction
-document.addEventListener('keydown', (e) => {
-    // Update direction based on key presses
-});
+// Function to create and display food
+function createFood(x, y) {
+    createPixel(x, y, 'food');
+}
 
-// Start the game loop
-setInterval(gameLoop, 100);
+// Function to initialize the game
+function initializeGame() {
+    // Create pixels and food here
+    createPixel(10, 10, 'pixel');
+    createPixel(20, 20, 'pixel');
+    createFood(5, 5);
+}
+
+// Initialize the game when the page loads
+window.onload = initializeGame;
